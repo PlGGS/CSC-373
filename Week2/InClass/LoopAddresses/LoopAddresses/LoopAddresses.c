01cc9d4382faee6c0091b7a516fd59c894736e35
Blake Boris <bbor0422@gmail.com>
2019-04-08 17:14:46 -0500

    Made in class work easier to follow and learn from

--- Week2/InClass/LoopAddresses/LoopAddresses/LoopAddresses.c
+++ Week2/InClass/LoopAddresses/LoopAddresses/LoopAddresses.c
@@ -1,19 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int Loop(int* ptr, int i) 
+///Loops through an declared in main without directly referencing it
+int Loop(int* ptr, int a) 
 {
-	for (*ptr = 0; *ptr <= 9; (*ptr)++) {
-		printf("%d\n", i);
+	for (*ptr = 0; *ptr <= 9; (*ptr)++)
+	{
+		printf("%d\n", a);
 	}
 }
 
 int main()
 {
-	int i = 3;
+	int a = 42;
+	int i = 0;
 	int* iPtr = &i;
 
-	Loop(iPtr, i);
+	Loop(iPtr, a);
 
 	char ch;
 	scanf_s(" %c", &ch); //pause
